6e393cbbdbd272c25605524bec25acb8358d9628
mgweaver <masongweaver@gmail.com>
2021-06-19 19:36:02 -0600

    port

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 
+const PORT = process.env.PORT || 5000
+
 app.set('view engine', 'ejs')
 .use(express.static(__dirname + '/public'))
 .use('/', require('./routes/route'))
-.listen(5000, () => {
+.listen(PORT, () => {
     console.log("Listening on port 5000")
 });
